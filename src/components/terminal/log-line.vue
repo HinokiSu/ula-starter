e1e457ee875ad91ecbf46c00ed063a36f9578844
HinokiSu <yukihinoki@qq.com>
2023-03-30 09:32:38 +0800

    feat: add terminal and log line component

---
+++ src/components/terminal/log-line.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="ula-log-line">
+    <component :is="vNode"></component>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { logObjToHtml, logToHtml } from '../../utils/log-to-html'
+
+export default defineComponent({
+  name: 'UlaLogLine',
+  setup() {
+    return {
+      vNode: logToHtml
+    }
+  }
+})
+</script>
+
+<style lang="less">
+/* if add scoped, style will not working. I currently don't know how to fix it */
+
+@--ula-log-error-color: #f10f5e;
+@--ula-log-warn-color: #faa823;
+@--ula-log-info-color: #19d96c;
+@--ula-log-http-color: #78e7be;
+@--ula-log-verbose-color: #cbc7c7;
+@--ula-log-debug-color: #9d48f3;
+@--ula-log-silly-color: #9ca5ab;
+
+.ula-log-line {
+    .log-tag {
+        font-size: 0.9rem;
+    }
+  .log-wrapper {
+    .log-timestamp {
+      font-size: 0.835rem;
+    }
+    .log-msg {
+      padding-left: 0.625rem;
+      font-family: var(--ula-fonts-sans);
+    }
+
+    /* error */
+    &.error {
+      .log-tag {
+        color: @--ula-log-error-color;
+      }
+
+      .log-timestamp {
+      }
+
+      .log-msg {
+      }
+    }
+
+    /* warn */
+    &.warn {
+      .log-tag {
+        color: @--ula-log-warn-color;
+      }
+
+      .log-timestamp {
+      }
+
+      .log-msg {
+      }
+    }
+
+    /* info */
+    &.info {
+      .log-tag {
+        color: @--ula-log-info-color;
+      }
+
+      .log-timestamp {
+      }
+
+      .log-msg {
+      }
+    }
+
+    /* http */
+    &.http {
+      .log-tag {
+        color: @--ula-log-http-color;
+      }
+
+      .log-timestamp {
+      }
+
+      .log-msg {
+      }
+    }
+
+    /* verbose */
+    &.verbose {
+      .log-tag {
+        color: @--ula-log-verbose-color;
+      }
+
+      .log-timestamp {
+      }
+
+      .log-msg {
+      }
+    }
+
+    /* debug */
+    &.debug {
+      .log-tag {
+        color: @--ula-log-debug-color;
+      }
+
+      .log-timestamp {
+      }
+
+      .log-msg {
+      }
+    }
+
+    /* silly */
+    &.silly {
+      .log-tag {
+        color: @--ula-log-silly-color;
+      }
+
+      .log-timestamp {
+      }
+
+      .log-msg {
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/terminal/terminal.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="ula-terminal-wrapper">
+    <div class="terminal-container">
+      <div class="terminal-header">
+        <span class="terminal-icon"></span>
+        <span class="terminal-icon"></span>
+        <span class="terminal-icon"></span>
+      </div>
+
+      <div class="terminal-body">
+        <ula-log-line></ula-log-line>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useLogStore } from '../../store/log-store'
+import UlaLogLine from './log-line.vue'
+export default defineComponent({
+  name: 'UlaTerminal',
+  components: {
+    UlaLogLine
+  },
+  setup() {
+    const logStore = useLogStore()
+
+    return {}
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.ula-terminal-wrapper {
+  margin-top: 1.5rem;
+  padding-bottom: 8px;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  height: 300px;
+  min-height: calc(65vh);
+}
+.terminal {
+  &-container {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    border-radius: 5px;
+    border: 1px solid #333;
+    background: #000;
+    color: #fff;
+    font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+      Bitstream Vera Sans Mono, Courier New, monospace;
+    line-height: 1.5;
+    font-size: 14px;
+    white-space: pre-wrap;
+    word-break: break-all;
+
+    background: #191724;
+    color: #e0def4;
+    border-color: #000;
+  }
+
+  &-header {
+    height: 34px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+  }
+
+  &-icon {
+    border-radius: 50%;
+    display: inline-block;
+    width: 12px;
+    height: 12px;
+
+    &:first-of-type {
+      background: #ff5f56;
+      margin-left: 12px;
+    }
+
+    &:nth-of-type(2) {
+      background: #ffbd2e;
+    }
+
+    &:nth-of-type(3) {
+      background: #27c93f;
+    }
+
+    & + .terminal-icon {
+      margin-left: 8px;
+    }
+  }
+
+  &-body {
+    padding: 12px 14px;
+    flex: 1 1;
+    overflow: auto;
+  }
+}
+
+/* Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 12px;
+}
+
+*::-webkit-scrollbar-track {
+  // background: #ffffff;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: #eeeded;
+  border-radius: 12px;
+  border: 4px solid #000;
+}
+
+*::-webkit-scrollbar:horizontal {
+  height: 12px;
+}
+
+*::-webkit-scrollbar-thumb:horizontal {
+  border-radius: 12px;
+  border: 4px solid #9b9898;
+  /* background-color: #e5e4e4; */
+}
+
+*::-webkit-scrollbar-thumb:horizontal:hover {
+  background-color: #363434;
+}
+</style>
