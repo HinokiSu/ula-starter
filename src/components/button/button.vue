0bef24e14319ee7b245a853f82610c2ca441cbae
HinokiSu <yukihinoki@qq.com>
2023-04-03 17:32:25 +0800

    feat: add button style and prefix icon

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="ula-button">
-    <div class="ula-button-wrapper">
-      <button class="ula-button" :disabled="disable" @click="clickHandler" :class="classes">
-        <span class="ula-button-text">
-          <slot></slot>
-        </span>
-      </button>
+  <button class="ula-button" :disabled="disable" @click="clickHandler" :class="classes">
+    <div class="button-prefix-icon" v-if="hasPreIconSlot">
+      <slot name="preIcon"></slot>
     </div>
-  </div>
+    <div class="ula-button-text">
+      {{ title }}
+    </div>
+  </button>
 </template>
 
 <script lang="ts">
@@ -16,6 +15,10 @@ import { computed, defineComponent } from 'vue'
 export default defineComponent({
   name: 'UlaButton',
   props: {
+    title: {
+      type: String,
+      default: ''
+    },
     color: {
       type: String,
       default: 'default'
@@ -30,7 +33,7 @@ export default defineComponent({
     }
   },
   emits: ['click'],
-  setup(props, { emit }) {
+  setup(props, { emit, slots }) {
     // TODO: Add more type button
     const propsRef = computed(() => props)
     const clickHandler = () => {
@@ -42,9 +45,12 @@ export default defineComponent({
       propsRef.value.active ? 'is-active' : ''
     ]
 
+    const hasPreIconSlot = computed(() => (slots.preIcon ? true : false))
+
     return {
       clickHandler,
-      classes
+      classes,
+      hasPreIconSlot
     }
   }
 })
@@ -52,111 +58,118 @@ export default defineComponent({
 
 <style lang="less" scoped>
 .ula-button {
-  padding: calc(2 * 0.375rem);
-}
-.ula-button-wrapper {
-  width: 100%;
-  margin: 0;
+  min-width: min-content;
+  width: auto;
 
-  .ula-button {
-    padding-left: 0.875rem;
-    padding-right: 0.875rem;
-    height: 2.5rem;
-    line-height: 2.5rem;
-    width: inherit;
-    font-size: 0.875rem;
+  padding-left: 0.875rem;
+  padding-right: 0.875rem;
 
-    color: #fff;
+  height: 2.5rem;
+  line-height: 2.5rem;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    appearance: none;
-    box-sizing: border-box;
-    font-weight: 500;
-    user-select: none;
-    text-align: center;
-    white-space: nowrap;
-    transition: background 0.25s ease 0s, color 0.25s ease 0s, border-color 0.25s ease 0s,
-      box-shadow 0.25s ease 0s, transform 0.25s ease 0s, opacity 0.25s ease 0s;
-    position: relative;
-    overflow: hidden;
-    border: none;
-    cursor: pointer;
-    pointer-events: auto;
-    padding: 0px;
-    border-radius: 12px;
+  font-size: 0.875rem;
+  font-weight: 500;
+  text-align: center;
+  color: #fff;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  appearance: none;
+  box-sizing: border-box;
+  user-select: none;
+  white-space: nowrap;
+  transition: background 0.25s ease 0s, color 0.25s ease 0s, border-color 0.25s ease 0s,
+    box-shadow 0.25s ease 0s, transform 0.25s ease 0s, opacity 0.25s ease 0s;
+  position: relative;
+  overflow: hidden;
+  border: none;
+  cursor: pointer;
+  pointer-events: auto;
+  padding: 0px;
+  border-radius: 12px;
+
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+  border-style: solid;
+  min-width: min-content;
+  border-width: 2px;
+
+  &.is-disabled {
+    background: #efeef0;
+    color: #7e868c;
+    transform: none;
+    box-shadow: none;
+    pointer-events: none;
+  }
+
+  &.default {
+    background-color: #eceef0;
+    color: #768e8c;
+  }
+
+  &.primary {
+    background: transparent;
+    border-color: var(--ula-colors-primary);
+    color: var(--ula-colors-primary);
     padding-left: 1.25rem;
     padding-right: 1.25rem;
     border-style: solid;
     min-width: min-content;
     border-width: 2px;
-
-    &.is-disabled {
-      background: #efeef0;
-      color: #7e868c;
-      transform: none;
-      box-shadow: none;
-      pointer-events: none;
-    }
-
-    &.default {
-      background-color: #eceef0;
-      color: #768e8c;
-    }
-
-    &.primary {
-      background: transparent;
-      border-color: var(--ula-colors-primary);
-      color: var(--ula-colors-primary);
-      padding-left: 1.25rem;
-      padding-right: 1.25rem;
-      border-style: solid;
-      min-width: min-content;
-      border-width: 2px;
-      &:hover {
-        color: #fff;
-        background: var(--ula-colors-primary);
-      }
-    }
-
-    &.gradient {
-      padding: 2px;
-      background-position: initial;
-      background-size: initial;
-      background-repeat: initial;
-      background-attachment: initial;
-      background-origin: initial;
-      background-color: transparent;
-      color: var(--ula-color-text);
-      background-clip: content-box, border-box;
-      background-image: linear-gradient(#fff, #fff), var(--ula-color-gradient);
-      border: none;
-
-      &:hover {
-        background: var(--ula-color-gradient);
-        color: #fff;
-      }
-
-      &.is-active {
-        background: var(--ula-color-gradient);
-        color: #fff;
-      }
+    &:hover {
+      color: #fff;
+      background: var(--ula-colors-primary);
     }
   }
 
-  .ula-button-text {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    z-index: 200;
-    padding-left: 0.875rem;
-    padding-right: 0.875rem;
-    line-height: 2.5rem;
+  &.gradient {
+    padding: 2px;
+    background-position: initial;
+    background-size: initial;
+    background-repeat: initial;
+    background-attachment: initial;
+    background-origin: initial;
+    background-color: transparent;
+    color: #444;
+    background-clip: content-box, border-box;
+    background-image: linear-gradient(#fff, #fff), var(--ula-color-gradient);
+    border: none;
 
-    font-size: 1rem;
-    font-family: var(--ula-fonts-sans);
+    &:hover {
+      background: var(--ula-color-gradient);
+      color: #fff;
+      .button-prefix-icon {
+        color: #fff;
+      }
+    }
+
+    &.is-active {
+      background: var(--ula-color-gradient);
+      color: #fff;
+    }
   }
+}
+
+.button-prefix-icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: inherit;
+  z-index: 100;
+  padding-left: 8px;
+}
+
+.ula-button-text {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 200;
+  padding-left: 0.875rem;
+  padding-right: 0.875rem;
+  line-height: 2.5rem;
+
+  font-size: 1rem;
+  font-family: var(--ula-fonts-sans);
 }
 </style>
