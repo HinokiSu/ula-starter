e293048e4ce37f3c97d928f5129238f7a678c4c5
HinokiSu <yukihinoki@qq.com>
2023-04-12 16:08:46 +0800

    chore: modify the way ula process in run

--- electron/services/index.ts
+++ electron/services/index.ts
@@ -1,20 +1,23 @@
 import { BrowserWindow, ipcMain } from 'electron'
-import UlaProcess from './run-ula'
+import { runUlaProcess, stopRunUla } from './run-ula'
 
 /* Main Process begin running ula.exe */
 export const bootstrap = () => {
-  const ulaProcess = new UlaProcess()
+  let pid: number
 
   // start
   ipcMain.handle('ula:start-run', () => {
-    const res = ulaProcess.runUlaProcess()
+    const res = runUlaProcess()
+    pid = res.pid
+    console.log('[ULA]: start run pid: ', pid)
     console.log('[ULA] start run')
     return res
   })
 
   // stop
   ipcMain.handle('ula:stop-run', () => {
-    const stopRes = ulaProcess.stopRunUla()
+    console.log('[ULA] stop pid: ', pid)
+    const stopRes = stopRunUla(pid)
     console.log('[ULA] stop result: ', stopRes)
     return stopRes
   })
@@ -45,7 +48,7 @@ export const requestTransitStation = (bgWin: BrowserWindow) => {
     console.log('[Polling]: start: ', data)
     bgWin.webContents.send('ula:start-polling-log', data)
   })
-  
+
   ipcMain.on('ula:stop-polling-log', (event, data) => {
     console.log('[Polling]: stop: ', data)
     bgWin.webContents.send('ula:stop-polling-log', data)
