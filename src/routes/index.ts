ccf9f755892f0d6e240fce170acdeedd776efdbc
HinokiSu <yukihinoki@qq.com>
2023-04-12 15:53:44 +0800

    fix: fix ula web page white screen bug in release

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   {
@@ -9,7 +9,8 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  // history: createWebHistory('/'),
+  history: createWebHashHistory('/'),
   routes
 })
 
